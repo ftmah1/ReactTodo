fb8a4cf20fe62927cd109f410883d5da606c9574
tanwir11 <tanwir11@bitbucket.org>
2017-09-11 13:44:33 +1000

    Add toggle support

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,54 +1,71 @@
-var React= require('react');
+var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
 var uuid = require('node-uuid');
 
 var TodoApp = React.createClass({
-  getInitialState: function(){
-    return{
+  getInitialState: function () {
+    return {
       showCompleted: false,
       searchText: '',
       todos: [
         {
           id: uuid(),
-          text: 'Walk the dog'
-        },{
+          text: 'Walk the dog',
+          completed: false
+        }, {
           id: uuid(),
-          text:'Clean the yard'
-        },{
+          text: 'Clean the yard',
+          completed: true
+        }, {
           id: uuid(),
-          text:'love'
-        },{
+          text: 'Leave mail on porch',
+          completed: true
+        }, {
           id: uuid(),
-          text:'Jia Xin Lai'
+          text: 'Play video games',
+          completed: false
         }
       ]
     };
   },
-  handleAddTodo: function(text){
+  handleAddTodo: function (text) {
     this.setState({
       todos: [
         ...this.state.todos,
         {
           id: uuid(),
-          text: text
+          text: text,
+          completed: false
         }
       ]
     });
   },
-  handleSearch: function(showCompleted,searchText){
+  handleToggle: function (id) {
+    var updatedTodos = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+
+      return todo;
+    });
+
+    this.setState({todos: updatedTodos});
+  },
+  handleSearch: function (showCompleted, searchText) {
     this.setState({
       showCompleted: showCompleted,
       searchText: searchText.toLowerCase()
     });
   },
-  render: function(){
+  render: function () {
     var {todos} = this.state;
-    return(
+
+    return (
       <div>
         <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={todos}/>
+        <TodoList todos={todos} onToggle={this.handleToggle}/>
         <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
     )
